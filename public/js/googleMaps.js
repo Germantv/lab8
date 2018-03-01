6b4a09e2e8ef1568324c53bddab2b13550a62a8d
German Flores <gfloresa@ucsd.edu>
2018-03-01 09:42:26 -0800

    lab8 - APIs

--- public/js/googleMaps.js
+++ public/js/googleMaps.js
@@ -1,3 +1,16 @@
 function initMap() {
-	// add your code here
+	//create center marker at UCSD
+	var ucsd_ltlng = {lat:32.8789248, lng:-117.2362174};
+
+	//create a map object and specify the DOM element for display
+	var map = new google.maps.Map(document.getElementById('map'), {
+		center: ucsd_ltlng,
+		zoom: 15
+	});
+
+	var marker = new google.maps.Marker({ 
+		position: ucsd_ltlng,
+		map: map, //'map' on right side of ':' referring to map object made above
+		title: "UCSD"
+	});
 }
--- routes/rsvp.js
+++ routes/rsvp.js
@@ -5,6 +5,15 @@ var data = {
 /*
  * GET tasks page.
  */
+ exports.addRSVP = function(req, res){
+	var rsvpEmail = req.body.rsvpEmail;
+	//this will print in your terminal when a POST is made
+	console.log(rsvpEmail);
+	data.rsvp.push(rsvpEmail); // add to current data
+	//lets send our task name back so our browser knows it worked out!
+	res.send(rsvpEmail);
+};
+
 exports.adminView = function(req, res){
   res.render('rsvp', data);
 };
